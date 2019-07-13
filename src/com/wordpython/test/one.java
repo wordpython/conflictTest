a3ad9d12105ca2cccbfb7d65d44979595e412ced
wordpython <wordpython@163.com>
2019-07-13 17:05:13 +0800

    eclipse中添加了新类three2，在类one中添加了first4方法

--- src/com/wordpython/test/one.java
+++ src/com/wordpython/test/one.java
@@ -10,7 +10,13 @@ public class one {
 	public void two() {
 		System.out.println("idea中写的第一个方法");
 	}
+	public void two1() {
+		System.out.println("idea中写的第二个方法");
+	}
 	public void first2() {
 		System.out.println("eclipse中写的方法,第3个方法");
+	}
+	public void first4() {
+		System.out.println("eclipse中写的方法,第4个方法");
 	}
 }
---
+++ src/com/wordpython/test/three2.java
@@ -0,0 +1,15 @@
+package com.wordpython.test;
+/*
+ * eclipse中创建的方类
+ */
+public class three2 {
+	public void first() {
+		System.out.println("eclipse中写的方法");
+	}
+	public void first1() {
+		System.out.println("eclipse中写的方法,第二个方法");
+	}
+	public void two() {
+		System.out.println("idea中写的第一个方法");
+	}
+}
